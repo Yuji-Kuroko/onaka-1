9d571d1a4f84538835aa1a4a345cf7917e83e6fe
yantene <contact@yantene.net>
2019-08-09 01:04:53 +0900

    余り意味のなさそうな UNAVAILABLE 某を削除

--- app/models/emoji.rb
+++ app/models/emoji.rb
@@ -12,10 +12,6 @@ require_relative './onaka'
 class Emoji < ActiveRecord::Base
   has_many :onakas
 
-  UNAVAILABLE_EMOJIS = %w[
-    onaka
-  ].freeze
-
   def self.update_emoji_list
     emoji_list = SLACK_CLIENT.web_client.emoji_list
 
@@ -24,8 +20,6 @@ class Emoji < ActiveRecord::Base
     # OPTIMIZE: timestamp 使って古いの消そう？
     ActiveRecord::Base.transaction do
       emoji_list['emoji'].each do |emoji_name, url|
-        next if UNAVAILABLE_EMOJIS.include?(emoji_name)
-
         onaka_name = url.start_with?('alias:') ? url.split(':')[1] : emoji_name
 
         onaka = Onaka.find_or_initialize_by(name: onaka_name)
