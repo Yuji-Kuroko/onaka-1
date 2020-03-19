65df1eb8b8df6f19c73ff7e5c2cc0d2500dbddef
cumet04 <medalhkr@gmail.com>
2020-03-19 09:45:39 +0900

    tune ranking format

--- app/onaka_bot/ranking.rb
+++ app/onaka_bot/ranking.rb
@@ -13,12 +13,28 @@ module OnakaBot
 
       post(
         User.order_by_score.take(10).map.with_index(1) { |u, rank|
-          "#{'%2dth' % rank}. #{u.name.nil? ? "_#{u.slack_id}_" : "*#{u.name}*"}   #{u.score}pts"
+          "#{rank_format(rank)}. #{u.name.nil? ? "_#{u.slack_id}_" : "*#{u.name}*"}   #{score_format(u.score)}pts"
         }.join("\n"),
         data,
       )
 
       true
+    end
+
+    private
+
+    def self.rank_format(num)
+      # 1st, 2nd, 3rd, 4th, ...
+      if num == 1; "1st"
+      elsif num == 2; "2nd"
+      elsif num == 3; "3rd"
+      else sprintf('%2dth', num)
+      end
+    end
+
+    def self.score_format(score)
+      # refs http://ruby.takayukikoyama.com/variable-constant-object/numeric-3-number-comma/
+      score.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse
     end
   end
 end
